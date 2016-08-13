f3a83fc7fd9d40032e5e2166ff064f8761a06fa1
David Tan <dtan@luc.edu>
2016-08-13 11:13:12 -0500

    add week1 decompositon of graphs1

---
+++ algorithmsongraphs/week1decompositionofgraphs1/connectedcomponents.scala
@@ -0,0 +1,61 @@
+import java.util
+import java.util.Scanner
+
+/**
+  * Created by davidtan on 8/10/16.
+  */
+object connectedcomponents {
+
+  private def dfs(vertex:Int, adj: Array[util.ArrayList[Int]], visited: Array[Boolean]):Unit={
+    visited(vertex) = true
+    import scala.collection.JavaConversions._
+    for (neighbor <- adj(vertex)) if (!visited(neighbor)) dfs(neighbor, adj, visited)
+  }
+
+  private def numberOfComponents(adj: Array[util.ArrayList[Int]]): Int = {
+    val visited: Array[Boolean] = new Array[Boolean](adj.length)
+    var result: Int = 1
+    for (i <- adj.indices) {
+      if (!visited(i)) {
+        dfs(i, adj, visited)
+        result += 1
+      }
+    }
+    result -1
+  }
+
+  def main(args: Array[String]) {
+    val scanner: Scanner = new Scanner(System.in)
+    val n: Int = scanner.nextInt
+    val m: Int = scanner.nextInt
+    val adj = new Array[util.ArrayList[Int]](n)
+
+    for (i <- 0 until n) adj(i) = new util.ArrayList[Int]()
+    for (i <- 0 until m) {
+      var x = 0
+      var y = 0
+      x = scanner.nextInt
+      y = scanner.nextInt
+      adj(x - 1).add(y - 1)
+      adj(y - 1).add(x - 1)
+    }
+
+    System.out.println(numberOfComponents(adj))
+    scanner.close()
+
+  }
+}
+/*
+10 August 2016
+Good job! (Max time used: 0.74/3.00, max memory used: 45596672/536870912.)
+
+4 2
+
+1 2
+
+3 2
+2
+
+///////
+
+ */
---
+++ algorithmsongraphs/week1decompositionofgraphs1/reachability1.scala
@@ -0,0 +1,75 @@
+import java.util
+import java.util.Scanner
+
+/**
+  * Created by davidtan on 8/10/16.
+  */
+object reachability1 {
+
+  private def reach(adj: Array[util.ArrayList[Int]], x: Int, y: Int): Int = {
+    val visited: Array[Boolean] = new Array[Boolean](adj.length)
+    dfs(adj, x, y, visited)
+  }
+
+  private def dfs(adj: Array[util.ArrayList[Int]], src: Int, dst: Int, visited: Array[Boolean]): Int = {
+    if (src == dst) return 1
+    visited(src) = true
+    import scala.collection.JavaConversions._
+    for (neighbor <- adj(src)) {
+      if (!visited(neighbor)) if (dfs(adj, neighbor, dst, visited) == 1) return 1
+    }
+    0
+  }
+  def main(args: Array[String]) {
+    val scanner: Scanner = new Scanner(System.in)
+    val n: Int = scanner.nextInt
+    val m: Int = scanner.nextInt
+    val adj = new Array[util.ArrayList[Int]](n)
+
+    for (i <- 0 until n) adj(i) = new util.ArrayList[Int]()
+    for (i <- 0 until m) {
+      var x = 0
+      var y = 0
+      x = scanner.nextInt
+      y = scanner.nextInt
+      adj(x - 1).add(y - 1)
+      adj(y - 1).add(x - 1)
+    }
+
+    val x: Int = scanner.nextInt - 1
+    val y: Int = scanner.nextInt - 1
+    System.out.println(reach(adj, x, y))
+    scanner.close
+
+
+  }
+}
+
+/*
+
+10 August 2016
+Good job! (Max time used: 0.76/3.00, max memory used: 45273088/536870912.)
+
+4 4
+
+1 2
+
+3 2
+
+4 3
+
+1 4
+
+1 4
+1
+
+//////
+4 2
+
+1 2
+
+3 2
+
+1 4
+0
+ */
