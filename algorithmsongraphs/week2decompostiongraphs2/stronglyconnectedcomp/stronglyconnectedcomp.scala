7b3f5e00d1c391df32f769e1d0166bb060c8dad0
David Tan <dtan@luc.edu>
2016-08-21 13:37:44 -0500

    add week2 graph assignments

---
+++ algorithmsongraphs/week2decompostiongraphs2/stronglyconnectedcomp/stronglyconnectedcomp.scala
@@ -0,0 +1,114 @@
+import java.util
+import java.util.Scanner
+
+/**
+  * Created by davidtan on 8/15/16.
+  */
+object stronglyconnectedcomp {
+
+  private def numberOfStronglyConnectedComponents(adj: Array[util.ArrayList[Int]]): Int = {
+    val adjR: Array[util.ArrayList[Int]] = reverseGraph(adj)
+    // DFS on GR for all source CCs in postorder.
+    val post: util.Stack[Int] = new util.Stack[Int]
+    var visited = new Array[Boolean](adj.length)
+    for (v<- adj.indices) {
+      if (!visited(v)) dfs(adjR, v, visited, post)
+    }
+    var res: Int = 0
+    visited = new Array[Boolean](adj.length)
+    // Reverse from max post source in GR, i.e. sink CC in G.
+    while (!post.isEmpty) {
+      val v: Int = post.pop
+      if (!visited(v)) {
+        dfs(adj, v, visited, null)
+        res += 1
+      }
+
+    }
+
+    res
+  }
+
+  private def reverseGraph(adj: Array[util.ArrayList[Int]]): Array[util.ArrayList[Int]] = {
+    /**Reverse G to GR: O(V+E)*/
+    val res = new Array[util.ArrayList[Int]](adj.length)
+    // Initialize empty adjacency list for each vertex. O(V)
+    for (v<- adj.indices) res(v) = new util.ArrayList[Int]
+    for (v<- adj.indices){
+      import scala.collection.JavaConversions._
+      for (w <- adj(v)) {
+        res(w).add(v)
+      }
+    }
+
+    res
+  }
+
+  private def dfs(adj: Array[util.ArrayList[Int]], v: Int, visited: Array[Boolean],
+                  stack: util.Stack[Int]) {
+    visited(v) = true
+    import scala.collection.JavaConversions._
+    for (w <- adj(v)) {
+      if (!visited(w)) {
+        dfs(adj, w, visited, stack)
+      }
+    }
+    if (stack != null) stack.push(v)
+  }
+
+
+  def main(args: Array[String]) {
+    val scanner: Scanner = new Scanner(System.in)
+    val n: Int = scanner.nextInt
+    val m: Int = scanner.nextInt
+    val adj = new Array[util.ArrayList[Int]](n)
+    for (i<-0 until n) {
+      adj(i) = new util.ArrayList[Int]
+    }
+    for (i<-0 until m) {
+      var x: Int = 0
+      var y: Int = 0
+      x = scanner.nextInt
+      y = scanner.nextInt
+      adj(x - 1).add(y - 1)
+    }
+    //println(adj.toList)//List([1], [2], [0], [0])
+    println(numberOfStronglyConnectedComponents(adj))
+  }
+}
+/*
+15 August 2016 at 6:27 PM
+Good job! (Max time used: 1.70/3.00, max memory used: 71094272/536870912.)
+4 4
+
+1 2
+
+4 1
+
+2 3
+
+3 1
+
+
+
+2
+/////
+
+
+
+2 1
+
+3 2
+
+3 1
+
+4 3
+
+4 1
+
+5 2
+
+5 3
+
+
+ */
---
+++ algorithmsongraphs/week2decompostiongraphs2/topological/topological1.scala
@@ -0,0 +1,97 @@
+import java.util
+import java.util.{Collections, Scanner}
+
+/**
+  * Created by davidtan on 8/14/16. //3 Problem: Determining an Order of Courses
+  *
+  *
+  * Now, when you are sure that there are no cyclic dependencies in the given CS curriculum, you would like to
+
+  *find an order of all courses that is consistent with all dependencies. For this, you find a topological ordering
+
+   *of the corresponding directed graph.
+  */
+object topological1 {
+  private def toposort(adj: Array[util.ArrayList[Int]]): util.ArrayList[Int] = {
+    /** DFS + reverse postorder() */
+    val used = new Array[Int](adj.length)
+    val order: util.ArrayList[Int] = new util.ArrayList[Int]
+    // Can store postorder into stack and pop all as well.
+    for (v<-adj.indices) {
+      if (used(v) == 0) dfs(adj, used, order, v)
+    }
+    Collections.reverse(order)
+    order
+  }
+  private def dfs(adj: Array[util.ArrayList[Int]], used: Array[Int],
+                  order: util.ArrayList[Int], s: Int): Unit = {
+    /** Add each current vertex after traversing all adjacent nodes. */
+    used(s) = 1
+    import scala.collection.JavaConversions._
+    for (t <- adj(s)) {
+      if (used(t) == 0) dfs(adj, used, order, t)
+    }
+    // Sort vertices based on postorder.
+    order.add(s)
+
+  }
+
+  ////////////////////////////////
+
+  def main(args: Array[String]) {
+    val scanner: Scanner = new Scanner(System.in)
+    val n: Int = scanner.nextInt
+    val m: Int = scanner.nextInt
+    val adj = new Array[util.ArrayList[Int]](n)
+    for (i<-0 until n) {
+      adj(i) = new util.ArrayList[Int]
+    }
+    for (i<-0 until m) {
+      var x: Int = 0
+      var y: Int = 0
+      x = scanner.nextInt
+      y = scanner.nextInt
+      adj(x - 1).add(y - 1)
+    }
+    //println(adj.toList)//List([1], [], [0], [0])
+
+    val order: util.ArrayList[Int] = toposort(adj)
+    import scala.collection.JavaConversions._
+    for (x <- order) {
+      System.out.print((x + 1) + " ")
+    }
+  }
+}
+/*
+15 August 2016 at 6:23 PM
+Good job! (Max time used: 0.93/3.00, max memory used: 45416448/536870912.)
+Output any topological ordering of its vertices.
+4 3
+
+1 2
+
+4 1
+
+3 1
+
+////
+4 3 1 2
+
+
+5 7
+
+2 1
+
+3 2
+
+3 1
+
+4 3
+
+4 1
+
+5 2
+
+5 3
+
+ */
